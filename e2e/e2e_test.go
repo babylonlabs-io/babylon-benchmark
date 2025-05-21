08099851cb6b3ad42556f8cc31af6ddac96198e4
Lazar <lukic.lazar@yahoo.com>
2025-05-21 16:13:41 +0200

    typo

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 func TestCreatesDelegations(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5§*time.Minute)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
 	defer cancel()
 
 	cfg := config.Config{
