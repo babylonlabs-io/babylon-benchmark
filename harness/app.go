ab47e8901d172a20e3b8398bc3f5a9f76728975a
Sam Ricotta <samanthalricotta@gmail.com>
2025-06-02 16:22:43 +0300

    wip

--- harness/app.go
+++ harness/app.go
@@ -3,11 +3,13 @@ package harness
 import (
 	"context"
 	"fmt"
-	"github.com/babylonlabs-io/babylon-benchmark/config"
-	"github.com/babylonlabs-io/babylon-benchmark/container"
-	"go.uber.org/zap"
 	"sync/atomic"
 	"time"
+
+	"github.com/babylonlabs-io/babylon-benchmark/config"
+	"github.com/babylonlabs-io/babylon-benchmark/container"
+	bncfg "github.com/babylonlabs-io/babylon/client/config"
+	"go.uber.org/zap"
 )
 
 var (
@@ -22,116 +24,128 @@ func Run(ctx context.Context, cfg config.Config) error {
 func startHarness(cmdCtx context.Context, cfg config.Config) error {
 	ctx, cancel := context.WithCancel(cmdCtx)
 	defer cancel()
-
 	numStakers := cfg.TotalStakers
 	numFinalityProviders := cfg.TotalFinalityProviders
 	stopChan := make(chan struct{}) // for stopping when we reach totalDelegations
 
-	tm, err := StartManager(ctx, cfg.NumMatureOutputs, 5, cfg)
-	if err != nil {
-		return err
-	}
-	defer tm.Stop()
+	if cfg.UseRemote {
+		_, err := NewBTCClient(cfg)
+		if err != nil {
+			return fmt.Errorf("error creating btc client %w", err)
+		}
+		bbncfg := bncfg.DefaultBabylonConfig()
+		_, err = New(&bbncfg)
+		if err != nil {
+			return fmt.Errorf("error creating babylon client %w", err)
+		}
+	} else {
 
-	// bold text
-	fmt.Printf("🟢 Starting with \033[1m%d\033[0m stakers, \u001B[1m%d\u001B[0m finality providers.\n", numStakers, numFinalityProviders)
+		tm, err := StartManager(ctx, cfg.NumMatureOutputs, 5, cfg)
+		if err != nil {
+			return err
+		}
+		defer tm.Stop()
 
-	cpSender, err := NewSenderWithBabylonClient(ctx, "node0", tm.Config.Babylon0.RPCAddr, tm.Config.Babylon0.GRPCAddr)
-	if err != nil {
-		return err
-	}
-	headerSender, err := NewSenderWithBabylonClient(ctx, "headerreporter", tm.Config.Babylon0.RPCAddr, tm.Config.Babylon0.GRPCAddr)
-	if err != nil {
-		return err
-	}
-	vigilanteSender, err := NewSenderWithBabylonClient(ctx, "vigilante", tm.Config.Babylon0.RPCAddr, tm.Config.Babylon0.GRPCAddr)
-	if err != nil {
-		return err
-	}
+		// bold text
+		fmt.Printf("🟢 Starting with \033[1m%d\033[0m stakers, \u001B[1m%d\u001B[0m finality providers.\n", numStakers, numFinalityProviders)
 
-	fpmSender, err := NewSenderWithBabylonClient(ctx, "fpmsender", tm.Config.Babylon0.RPCAddr, tm.Config.Babylon0.GRPCAddr)
-	if err != nil {
-		return err
-	}
-
-	if err := tm.fundAllParties(ctx, []*SenderWithBabylonClient{cpSender, headerSender, vigilanteSender, fpmSender}); err != nil {
-		return err
-	}
-
-	fpMgrHome, err := tempDir()
-	if err != nil {
-		return err
-	}
-	defer cleanupDir(fpMgrHome)
-
-	eotsDir, err := tempDir()
-	if err != nil {
-		return err
-	}
-	defer cleanupDir(eotsDir)
-
-	gen := NewBTCHeaderGenerator(tm, headerSender)
-	gen.Start(ctx)
-
-	vig := NewSubReporter(tm, vigilanteSender)
-	vig.Start(ctx)
-
-	fpMgr := NewFinalityProviderManager(tm, fpmSender, zap.NewNop(), numFinalityProviders, fpMgrHome, eotsDir)
-	if err = fpMgr.Initialize(ctx, cfg.NumPubRand); err != nil {
-		return err
-	}
-
-	var stakers []*BTCStaker
-	for i := 0; i < numStakers; i++ {
-		stakerSender, err := NewSenderWithBabylonClient(ctx, fmt.Sprintf("staker-%d", i), tm.Config.Babylon1.RPCAddr, tm.Config.Babylon1.GRPCAddr)
+		cpSender, err := NewSenderWithBabylonClient(ctx, "node0", tm.Config.Babylon0.RPCAddr, tm.Config.Babylon0.GRPCAddr)
+		if err != nil {
+			return err
+		}
+		headerSender, err := NewSenderWithBabylonClient(ctx, "headerreporter", tm.Config.Babylon0.RPCAddr, tm.Config.Babylon0.GRPCAddr)
+		if err != nil {
+			return err
+		}
+		vigilanteSender, err := NewSenderWithBabylonClient(ctx, "vigilante", tm.Config.Babylon0.RPCAddr, tm.Config.Babylon0.GRPCAddr)
 		if err != nil {
 			return err
 		}
 
-		rndFpChunk := fpMgr.getRandomChunk(3)
+		fpmSender, err := NewSenderWithBabylonClient(ctx, "fpmsender", tm.Config.Babylon0.RPCAddr, tm.Config.Babylon0.GRPCAddr)
+		if err != nil {
+			return err
+		}
 
-		stakers = append(stakers, NewBTCStaker(tm, stakerSender, rndFpChunk, tm.fundingRequests, tm.fundingResponse))
+		if err := tm.fundAllParties(ctx, []*SenderWithBabylonClient{cpSender, headerSender, vigilanteSender, fpmSender}); err != nil {
+			return err
+		}
+
+		fpMgrHome, err := tempDir()
+		if err != nil {
+			return err
+		}
+		defer cleanupDir(fpMgrHome)
+
+		eotsDir, err := tempDir()
+		if err != nil {
+			return err
+		}
+		defer cleanupDir(eotsDir)
+
+		gen := NewBTCHeaderGenerator(tm, headerSender)
+		gen.Start(ctx)
+
+		vig := NewSubReporter(tm, vigilanteSender)
+		vig.Start(ctx)
+
+		fpMgr := NewFinalityProviderManager(tm, fpmSender, zap.NewNop(), numFinalityProviders, fpMgrHome, eotsDir)
+		if err = fpMgr.Initialize(ctx, cfg.NumPubRand); err != nil {
+			return err
+		}
+
+		var stakers []*BTCStaker
+		for i := 0; i < numStakers; i++ {
+			stakerSender, err := NewSenderWithBabylonClient(ctx, fmt.Sprintf("staker-%d", i), tm.Config.Babylon1.RPCAddr, tm.Config.Babylon1.GRPCAddr)
+			if err != nil {
+				return err
+			}
+
+			rndFpChunk := fpMgr.getRandomChunk(3)
+
+			stakers = append(stakers, NewBTCStaker(tm, stakerSender, rndFpChunk, tm.fundingRequests, tm.fundingResponse))
+		}
+
+		// periodically check if we need to fund the staker
+		go tm.fundForever(ctx)
+
+		// fund all stakers
+		if err := tm.fundAllParties(ctx, senders(stakers)); err != nil {
+			return err
+		}
+
+		// start stakers
+		if err := startStakersInBatches(ctx, stakers); err != nil {
+			return err
+		}
+
+		go printStatsForever(ctx, tm, stopChan, cfg)
+
+		covenantSender, err := NewSenderWithBabylonClient(ctx, "covenant", tm.Config.Babylon0.RPCAddr, tm.Config.Babylon0.GRPCAddr)
+		if err != nil {
+			return err
+		}
+		covenant := NewCovenantEmulator(tm, container.CovenantPrivKey, covenantSender)
+		if err := tm.fundAllParties(ctx, []*SenderWithBabylonClient{covenantSender}); err != nil {
+			return err
+		}
+
+		covenant.Start(ctx)
+
+		// finality providers start voting
+		fpMgr.Start(ctx)
+
+		go tm.listBlocksForever(ctx)
+
+		select {
+		case <-ctx.Done():
+		case <-stopChan:
+			return nil
+		}
 	}
-
-	// periodically check if we need to fund the staker
-	go tm.fundForever(ctx)
-
-	// fund all stakers
-	if err := tm.fundAllParties(ctx, senders(stakers)); err != nil {
-		return err
-	}
-
-	// start stakers
-	if err := startStakersInBatches(ctx, stakers); err != nil {
-		return err
-	}
-
-	go printStatsForever(ctx, tm, stopChan, cfg)
-
-	covenantSender, err := NewSenderWithBabylonClient(ctx, "covenant", tm.Config.Babylon0.RPCAddr, tm.Config.Babylon0.GRPCAddr)
-	if err != nil {
-		return err
-	}
-	covenant := NewCovenantEmulator(tm, container.CovenantPrivKey, covenantSender)
-	if err := tm.fundAllParties(ctx, []*SenderWithBabylonClient{covenantSender}); err != nil {
-		return err
-	}
-
-	covenant.Start(ctx)
-
-	// finality providers start voting
-	fpMgr.Start(ctx)
-
-	go tm.listBlocksForever(ctx)
-
-	select {
-	case <-ctx.Done():
-	case <-stopChan:
-		return nil
-	}
-
 	return nil
 }
+
 func printStatsForever(ctx context.Context, tm *TestManager, stopChan chan struct{}, cfg config.Config) {
 	t := time.NewTicker(5 * time.Second)
 	defer t.Stop()
