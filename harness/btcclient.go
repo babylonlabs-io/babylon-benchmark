a7d4aaa71b1015d63a9e66dae5b716a1bc9d58cf
samricotta <37125168+samricotta@users.noreply.github.com>
2025-06-13 15:41:33 +0300

    chore: add btcclient (#55)
    * Create btcclient.go
    
    * adding btc client, bbn client and import for bbn
    
    * Revert "adding btc client, bbn client and import for bbn"
    
    This reverts commit 5bd4dd847d8177ce9700bf460e55eadbaf0a83a0.
    
    * comments
    
    * update client values
    
    * Update btcclient.go
    
    * Update btcclient.go
    
    * change name

---
+++ harness/btcclient.go
@@ -0,0 +1,61 @@
+package harness
+
+import (
+	"github.com/babylonlabs-io/babylon-benchmark/config"
+
+	"github.com/btcsuite/btcd/rpcclient"
+)
+
+type BTCClient struct {
+	client *rpcclient.Client
+	config config.BTCConfig
+}
+
+func NewBTCClient(cfg config.BTCConfig) (*BTCClient, error) {
+	client := &BTCClient{
+		config: cfg,
+	}
+	return client, nil
+}
+
+func (c *BTCClient) Setup(cfg config.Config) error {
+	c.convertToBTCConfig(cfg)
+	rpcClient, err := rpcclient.New(&rpcclient.ConnConfig{
+		Host:         rpcHostURL(c.config.Endpoint, c.config.WalletName),
+		User:         c.config.Username,
+		Pass:         c.config.Password,
+		DisableTLS:   true,
+		HTTPPostMode: true,
+	}, nil)
+	if err != nil {
+		return err
+	}
+
+	c.client = rpcClient
+
+	return nil
+}
+
+func rpcHostURL(host, walletName string) string {
+	if len(walletName) > 0 {
+		return host + "/wallet/" + walletName
+	}
+
+	return host
+}
+
+func (c *BTCClient) Stop() {
+	if c.client != nil {
+		c.client.Shutdown()
+	}
+}
+
+func (c *BTCClient) convertToBTCConfig(cfg config.Config) config.BTCConfig {
+	return config.BTCConfig{
+		Endpoint:       cfg.BTCRPC,
+		WalletName:     cfg.WalletName,
+		WalletPassword: cfg.WalletPassphrase,
+		Username:       cfg.BTCUser,
+		Password:       cfg.BTCPass,
+	}
+}
